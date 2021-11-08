d1069e732e14e0a455cb76702e60543c181bab97
diegomarrufo <dmarrufo@ucm.es>
2021-11-08 12:26:58 +0100

    done, can fix it up a little bit with css

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def ask
+  end
+  def answer
+    @response = ""
+    if params[:question].downcase == "i am going to work right now!"
+      @response = ""
+    elsif params[:question].end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
